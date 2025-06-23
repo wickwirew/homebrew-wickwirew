196a51f7714985aff9a7928244e9ce694aaebc82
wes <wickwirew@gmail.com>
2025-06-22 20:54:24 -0400

    Fixed path

--- Formula/otter.rb
+++ Formula/otter.rb
@@ -16,7 +16,7 @@ class Otter < Formula
     end
 
     system "swift", "build", *args, "-c", "release", "--product", "OtterCLI"
-    system "cp", ".build/release/OtterCLI", "./build/release/otter"
+    system "cp", ".build/release/OtterCLI", ".build/release/otter"
     bin.install ".build/release/otter"
   end
 
