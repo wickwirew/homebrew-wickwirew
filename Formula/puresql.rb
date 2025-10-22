fd3c8381947504bfe8c7c1a655c12ba20a4c3479
wes <wickwirew@gmail.com>
2025-10-21 21:42:19 -0400

    maybe

--- Formula/puresql.rb
+++ Formula/puresql.rb
@@ -1,12 +1,10 @@
 class Puresql < Formula
-  desc "A SQLite compiler for Swift"
+  desc "SQLite compiler for Swift"
   homepage "https://github.com/wickwirew/PureSQL"
-  license "MIT"
   url "https://github.com/wickwirew/PureSQL/archive/refs/tags/0.1.0.tar.gz"
   sha256 "e5fe34b9abca359bfc65b2371e8a8c79bcec398c748b7728186d074316bc24c5"
-  version "0.1.0"
-  head "https://github.com/wickwirew/PureSQL.git", branch: "main"
-
+  license "MIT"
+  
   depends_on xcode: ["16.4", :build]
 
   uses_from_macos "swift" => :build
@@ -19,11 +17,11 @@ class Puresql < Formula
     end
 
     system "swift", "build", *args, "-c", "release", "--product", "PureSQLCLI"
-    system "cp", ".build/release/PureSQLCLI", ".build/release/puresql"
+    cp, ".build/release/PureSQLCLI", ".build/release/puresql"
     bin.install ".build/release/puresql"
   end
 
   test do
-    puresql --help
+    system bin/"puresql", "--help"
   end
 end
