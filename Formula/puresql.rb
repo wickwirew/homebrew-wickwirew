9266fe4915e9600bbbe1ee71ea46d25cc576dc63
wes <wickwirew@gmail.com>
2025-10-21 21:35:00 -0400

    Updated formula

--- Formula/puresql.rb
+++ Formula/puresql.rb
@@ -2,9 +2,12 @@ class Puresql < Formula
   desc "A SQLite compiler for Swift"
   homepage "https://github.com/wickwirew/PureSQL"
   license "MIT"
+  url "https://github.com/wickwirew/PureSQL/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "e5fe34b9abca359bfc65b2371e8a8c79bcec398c748b7728186d074316bc24c5"
+  version "0.1.0"
   head "https://github.com/wickwirew/PureSQL.git", branch: "main"
 
-  depends_on xcode: ["16.3", :build]
+  depends_on xcode: ["16.4", :build]
 
   uses_from_macos "swift" => :build
 
