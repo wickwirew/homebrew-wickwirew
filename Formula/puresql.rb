6c4581515ac9ef3545a00bfcefc9711b8ca36e49
Wes Wickwire <wickwirew@gmail.com>
2025-10-22 03:05:08 +0000

    puresql: add 0.1.0 bottle.

--- Formula/puresql.rb
+++ Formula/puresql.rb
@@ -5,6 +5,13 @@ class Puresql < Formula
   sha256 "e5fe34b9abca359bfc65b2371e8a8c79bcec398c748b7728186d074316bc24c5"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/wickwirew/homebrew-wickwirew/releases/download/puresql-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8bdb4420969905c9846712bbe28ea894e520d8c8966df8ce92da275775f7d05d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04f904da3adfa46343ea38f28a704621efe5a6b123e1bf10a02fc22c09f11e79"
+  end
+
   depends_on xcode: ["16.4", :build]
 
   uses_from_macos "swift" => :build
